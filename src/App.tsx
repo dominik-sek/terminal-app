3f52b599bcf0bec3076b398489407ceee54c9286
Dominik Sęk <d.sek464@gmail.com>
2023-03-19 12:22:17 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+function App() {
+  return (
+    <div className="App">
+        <h1 className="bg-orange-200 font-bold p-4">Vite React Tailwind Eslint Prettier</h1>
+    </div>
+  )
+}
+
+export default App
